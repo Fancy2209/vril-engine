9be96832ad91a6b24cb8a319728ade694e877871
Fancy2209 <paulo.medeiros1302@gmail.com>
2025-11-30 16:38:27 -0100

    Swap Circle and Cross on menu

--- source/platform/ps3/menu.c
+++ source/platform/ps3/menu.c
@@ -266,7 +266,7 @@ void M_Start_Key (int key)
 {
 	switch (key)
 	{
-		case K_AUX1:
+		case K_AUX2:
 			S_LocalSound ("sounds/menu/enter.wav");
 			//Cbuf_AddText("cd playstring tensioned_by_the_damned 1\n");
 			Cbuf_AddText("togglemenu\n");
@@ -371,7 +371,7 @@ static void M_Paused_Menu_Key (int key)
 	switch (key)
 	{
 		case K_ESCAPE:
-		case K_AUX2:
+		case K_AUX1:
 			S_LocalSound ("sounds/menu/enter.wav");
 			Cbuf_AddText("togglemenu\n");
 			break;
@@ -389,7 +389,7 @@ static void M_Paused_Menu_Key (int key)
 			break;
 
 		case K_ENTER:
-		case K_AUX1:
+		case K_AUX2:
 			m_entersound = true;
 
 			switch (M_Paused_Cusor)
@@ -528,7 +528,7 @@ void M_Main_Key (int key)
 		break;
 
 	case K_ENTER:
-	case K_AUX1:
+	case K_AUX2:
 		m_entersound = true;
 
 		switch (m_main_cursor)
@@ -605,8 +605,8 @@ void M_Credits_Key (int key)
 	switch (key)
 	{
 		case K_ENTER:
-		case K_AUX1:
-        case K_AUX2:
+		case K_AUX2:
+        case K_AUX1:
             M_Menu_Main_f ();
             break;
 	}
@@ -642,7 +642,7 @@ void M_Restart_Key (int key)
 	switch (key)
 	{
 	case K_ESCAPE:
-	case K_AUX2:
+	case K_AUX1:
 	case 'n':
 	case 'N':
 		m_state = m_paused_menu;
@@ -652,7 +652,7 @@ void M_Restart_Key (int key)
 	case 'Y':
 	case 'y':
 	case K_ENTER:
-	case K_AUX1:
+	case K_AUX2:
 		key_dest = key_game;
 		m_state = m_none;
 		// Cbuf_AddText ("restart\n"); // nai -- old, now do soft reset
@@ -711,7 +711,7 @@ void M_Exit_Key (int key)
 	switch (key)
 	{
 	case K_ESCAPE:
-	case K_AUX2:
+	case K_AUX1:
 	case 'n':
 	case 'N':
 		m_state = m_paused_menu;
@@ -721,7 +721,7 @@ void M_Exit_Key (int key)
 	case 'Y':
 	case 'y':
 	case K_ENTER:
-	case K_AUX1:
+	case K_AUX2:
 		Cbuf_AddText("disconnect\n");
 		CL_ClearState ();
 		M_Menu_Main_f();
@@ -886,7 +886,7 @@ void M_SinglePlayer_Key (int key)
 		break;
 
 	case K_ENTER:
-	case K_AUX1:
+	case K_AUX2:
 		m_entersound = true;
 
 		switch (m_singleplayer_cursor)
@@ -944,8 +944,8 @@ void M_SinglePlayer_Key (int key)
 		}
 		break;
 
-	// b button
-	case K_AUX2:
+	// x button
+	case K_AUX1:
 		M_Menu_Main_f();
 		break;
 	}
@@ -1230,7 +1230,7 @@ void M_Menu_CustomMaps_Key (int key)
 	switch (key)
 	{
 		case K_ESCAPE:
-		case K_AUX2:
+		case K_AUX1:
 			M_Menu_SinglePlayer_f ();
 			break;
 		case K_DOWNARROW:
@@ -1275,7 +1275,7 @@ void M_Menu_CustomMaps_Key (int key)
 			}
 			break;
 		case K_ENTER:
-		case K_AUX1:
+		case K_AUX2:
 			m_entersound = true;
 			if (m_map_cursor == 17) {
 				M_Menu_SinglePlayer_f ();
@@ -1485,7 +1485,7 @@ void M_Options_Key (int k)
 	switch (k)
 	{
 	case K_ENTER:
-	case K_AUX1:
+	case K_AUX2:
 		m_entersound = true;
 		switch (options_cursor)
 		{
@@ -1533,7 +1533,7 @@ void M_Options_Key (int k)
 		break;
 
 	case K_ESCAPE:
-	case K_AUX2:
+	case K_AUX1:
 		if (key_dest == key_menu_pause)
 			M_Paused_Menu_f();
 		else
@@ -1705,7 +1705,7 @@ void M_Keys_Key (int k)
 	switch (k)
 	{
 	case K_ESCAPE:
-	case K_AUX2:
+	case K_AUX1:
 		M_Menu_Options_f();
 		break;
 
@@ -1726,7 +1726,7 @@ void M_Keys_Key (int k)
 		break;
 
 	case K_ENTER:		// go into bind mode
-	case K_AUX1:
+	case K_AUX2:
 		M_FindKeysForCommand (bindnames[keys_cursor][0], keys);
 		S_LocalSound ("misc/menu2.wav");
 		if (keys[1] != -1)
@@ -1800,11 +1800,11 @@ void M_Quit_Key (int key)
 	switch (key)
 	{
 
-	case K_AUX2:
+	case K_AUX1:
 		M_Menu_Main_f();
 		break;
 
-	case K_AUX1:
+	case K_AUX2:
 		game_running = false;
 		break;
 	}
@@ -2195,7 +2195,7 @@ void M_GameOptions_Key (int key)
 		break;
 
 	case K_ENTER:
-	case K_AUX1:
+	case K_AUX2:
 		S_LocalSound ("misc/menu2.wav");
 		if (gameoptions_cursor == 0)
 		{
