c71b9975b459f8e696d82ced922408fbc1497c41
Fancy2209 <64917206+Fancy2209@users.noreply.github.com>
2026-02-18 11:46:42 -0100

    Missing change

--- source/sv_main.c
+++ source/sv_main.c
@@ -1165,6 +1165,7 @@ void SV_SpawnServer (char *server)
 {
 	edict_t		*ent;
 	int			i;
+	static char	dummy[8] = { 0,0,0,0,0,0,0,0 };
 
 	// let's not have any servers with no name
 	if (hostname.string[0] == 0)
@@ -1254,9 +1255,9 @@ void SV_SpawnServer (char *server)
 //
 	SV_ClearWorld ();
 
-	sv.sound_precache[0] = pr_strings;
+	sv.sound_precache[0] = dummy;
 
-	sv.model_precache[0] = pr_strings;
+	sv.model_precache[0] = dummy;
 	sv.model_precache[1] = sv.modelname;
 	for (i=1 ; i<sv.worldmodel->numsubmodels ; i++)
 	{
